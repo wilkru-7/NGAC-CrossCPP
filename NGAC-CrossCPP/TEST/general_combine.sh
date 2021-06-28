c2fc24ddce3e4802f43c3ba46e654878916d6f70
André Christofferson <andrechristofferson@hotmail.com>
2021-06-28 15:38:22 +0200

    .

--- NGAC-CrossCPP/TEST/general_combine.sh
+++ NGAC-CrossCPP/TEST/general_combine.sh
@@ -1,5 +1,7 @@
 echo 'Combining policies'
-curl -s "http://127.0.0.1:8001/paapi/combinepol?policy1=users&policy2=general&combined=combined_policies&token=admin_token"
+curl -s "http://127.0.0.1:8001/paapi/combinepol?policy1=users&policy2=general_cond&combined=combined_policies&token=admin_token"
+#curl -s "http://127.0.0.1:8001/paapi/combinepol?policy1=combined_policies&policy2=general_cond&combined=combined_policies2&token=admin_token"
+
 
 echo '\nSet the policy to combined policy '
 curl -s -G "http://127.0.0.1:8001/paapi/setpol?policy=combined_policies" --data-urlencode "token=admin_token"
@@ -14,5 +16,6 @@ curl -s "http://127.0.0.1:8001/pqapi/access?user=u4&object=o2&ar=book"
 
 curl -s "http://127.0.0.1:8001/pqapi/access?user=u1&object=o2&ar=book"
 curl -s "http://127.0.0.1:8001/pqapi/access?user=u2&object=o1&ar=book"
-
+curl -s -g "http://127.0.0.1:8001/pqapi/access?user=u1&object=o1&ar=book&cond=[location_user=siteA,location_object=siteB]"
+curl -s -i'http://127.0.0.1:8001/pqapi/access?user=u1&object=o1&ar=book&cond=is_same_site(site, site)'
 
--- NGAC-CrossCPP/policies.pl
+++ NGAC-CrossCPP/policies.pl
@@ -752,7 +752,6 @@ policy(general, 'Defining Objects', [
 	associate('GroupA', [book], 'SiteA'),
 	associate('GroupB', [book], 'SiteB'),
 	associate('GroupC', [book], 'Sites')
-
 	]).
 
 policy(test9,'test9', [
@@ -839,6 +838,34 @@ policy(cond_associates, 'Defining conditional operations', [
 	% assign('Defining', 'PM'),
 
 	cond(is_same_site(location_user, location_object), [
+		associate('GroupA',[book],'SiteA'),
+		associate('GroupB',[book],'SiteB'),
+		associate('GroupC',[book],'Sites')])
+	]).
+
+policy(general_cond, 'Defining Objects Cond', [
+	object('o1'),
+	object('o2'),
+
+	object_attribute('Sites'),
+	object_attribute('SiteA'),
+	object_attribute('SiteB'),
+
+	policy_class('Defining'),
+	connector('PM'),
+
+	assign('o1', 'SiteA'),
+	assign('o2', 'SiteB'),
+
+	assign('SiteA', 'Sites'),
+	assign('SiteB', 'Sites'),
+
+	assign('Sites', 'Defining'),
+	%assign('Sites', 'PM'),
+	assign('Defining', 'PM'),
+
+	
+	cond(is_same_site(_, _), [
 		associate('GroupA',[book],'SiteA'),
 		associate('GroupB',[book],'SiteB'),
 		associate('GroupC',[book],'Sites')])
