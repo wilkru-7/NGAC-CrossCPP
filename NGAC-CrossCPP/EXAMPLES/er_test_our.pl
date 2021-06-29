935ad24e0d3187a0e187b7ee40d1df2f71a945d0
André Christofferson <andrechristofferson@hotmail.com>
2021-06-29 08:23:54 +0200

    Update er_test_our.pl

--- NGAC-CrossCPP/EXAMPLES/er_test_our.pl
+++ NGAC-CrossCPP/EXAMPLES/er_test_our.pl
@@ -6,5 +6,25 @@ er_package(er_example1, [
         ev_pat(user(any), policy_class(any), operation(add_user), object(any)), [
             add(example3, user(u5))
         
-        ])
+        ]),
+    er( 
+        ev_pat(user(any), policy_class(any), operation(add_object), object(any)), [
+            add(example3, object(o5))
+    
+    ]),
+    er( 
+        ev_pat(user(any), policy_class(any), operation(add_objects), object(any)), [
+            add(example3, object(o6)),
+            add(example3, object(o7))
+    
+    ]),
+    er( 
+        ev_pat(user(any), policy_class(any), operation(add_assignment), object(any)), [
+            %user_attribute('admin'),
+            %assign(u1, admin)
+            addm(example3,[
+                user_attribute(admin),
+                assign(u1, admin)
+               ])
+    ])
 ]).
