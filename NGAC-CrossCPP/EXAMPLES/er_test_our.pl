659a083941c7bb44b376139b60037eff34836000
André Christofferson <andrechristofferson@hotmail.com>
2021-06-29 08:01:18 +0200

    Update er_test_our.pl

--- NGAC-CrossCPP/EXAMPLES/er_test_our.pl
+++ NGAC-CrossCPP/EXAMPLES/er_test_our.pl
@@ -1,6 +1,10 @@
+/* After loading this er package this response can be invoked by 
+making the following call to the server:
+localhost:8001/epp/report_event?event=event(test_event,user(u1),policy_class(pc),operation(add_user),object(o1))&token=epp_token */
 er_package(er_example1, [
     er( 
         ev_pat(user(any), policy_class(any), operation(add_user), object(any)), [
-            add('example3', [user(u5)])
+            add(example3, user(u5))
+        
         ])
 ]).
