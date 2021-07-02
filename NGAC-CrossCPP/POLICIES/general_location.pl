d7758df2478026c9bdb7c2153418ae4cd0b4f02d
Wilma <wilkru-7@student.ltu.se>
2021-07-02 08:55:00 +0200

    Added SiteC

--- NGAC-CrossCPP/POLICIES/general_location.pl
+++ NGAC-CrossCPP/POLICIES/general_location.pl
@@ -3,19 +3,23 @@
 policy(general_cond_location, 'Defining Objects Cond', [
 	object('o1'),
 	object('o2'),
+	object('o3'),
 
 	object_attribute('Sites'),
 	object_attribute('SiteA'),
 	object_attribute('SiteB'),
+	object_attribute('SiteC'),
 
 	policy_class('Defining'),
 	connector('PM'),
 
 	assign('o1', 'SiteA'),
 	assign('o2', 'SiteB'),
+	assign('o3', 'SiteC'),
 
 	assign('SiteA', 'Sites'),
 	assign('SiteB', 'Sites'),
+	assign('SiteC', 'Sites'),
 
 	assign('Sites', 'Defining'),
 	%assign('Sites', 'PM'),
