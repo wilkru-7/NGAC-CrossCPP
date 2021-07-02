558b806d7372cdaa5e667e816ce0823b408bce89
Wilma <wilkru-7@student.ltu.se>
2021-07-02 08:53:46 +0200

    test for level

--- NGAC-CrossCPP/TEST/RTB-tests/json_location.sh
+++ NGAC-CrossCPP/TEST/RTB-tests/json_location.sh
@@ -14,7 +14,7 @@ curl -s -G "http://127.0.0.1:8001/paapi/setpol?policy=combined_policies" --data-
 echo '\nSet Access query - G G G G'
 curl -s "http://127.0.0.1:8001/pqapi/access?user=u1&object=o1&ar=book&cond=is_same_location('\"u1\"','\"o1\"')"
 curl -s "http://127.0.0.1:8001/pqapi/access?user=u2&object=o2&ar=book&cond=is_same_location('\"u2\"','\"o2\"')"
-curl -s "http://127.0.0.1:8001/pqapi/access?user=u3&object=o2&ar=book&cond=is_same_location('\"u3\"','\"o2\"')"
+curl -s "http://127.0.0.1:8001/pqapi/access?user=u3&object=o3&ar=book&cond=is_same_location('\"u3\"','\"o3\"')"
 curl -s "http://127.0.0.1:8001/pqapi/access?user=u4&object=o2&ar=book&cond=is_same_location('\"u4\"','\"o2\"')"
 
 echo '\nSet Access query - D D D D'
